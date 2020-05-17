a5da1a87c38f217c722213e9f25aba429530293c
Miłosz Mika <milosz.mika93@gmail.com>
2020-05-17 19:33:02 +0200

    test git repo on slack

---
+++ PortalApi/Models/SampleClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalApi.Models
+{
+    public class SampleClass
+    {
+        public int Id { get; set; }
+    }
+}
