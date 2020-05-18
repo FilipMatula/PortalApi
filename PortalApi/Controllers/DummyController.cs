78854d85449c223cff115fae9cbe0bb31067c67c
Filip M <siwy1296@o2.pl>
2020-05-18 17:54:42 +0200

    octa part 2

--- PortalApi/Controllers/DummyController.cs
+++ PortalApi/Controllers/DummyController.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PortalApi.Contexts;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace PortalApi.Controllers
