ed828e476734dfd52087fefd07aa2257f8d0277d
Miłosz Mika <milosz.mika93@gmail.com>
2020-05-24 22:00:31 +0200

    Add models skeleton

---
+++ PortalApi/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalApi.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MailAdress { get; set; }
+        public string Profession { get; set; }
+        public string Experiance { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+        public string PathImg { get; set; }
+        public string TelephoneNummber { get; set; }
+        public string Gender{ get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ PortalApi/Models/Piercer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalApi.Models
+{
+    public class Piercer : Person
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+
+        public int AssignedToStudioId { get; set; }
+        public Studio Studio { get; set; }
+    }
+}
