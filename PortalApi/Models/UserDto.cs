adbc83cf18e1359c219a722d94de7dc491419280
Filip M <siwy1296@o2.pl>
2020-06-12 10:35:47 +0200

    - change

--- PortalApi/Models/UserDto.cs
+++ PortalApi/Models/UserDto.cs
@@ -13,6 +13,7 @@ namespace PortalApi.Models
         public string Username { get; set; }
         public string City { get; set; }
         public string Gender { get; set; }
+        public int Age { get; set; }
         public string ImgSrc { get; set; }
     }
 }
