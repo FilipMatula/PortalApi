a7cb64a4cbf13454bb29de978b04e7408d1458ad
Filip M <siwy1296@o2.pl>
2020-06-30 21:31:54 +0200

    - changed cors

--- PortalApi/Startup.cs
+++ PortalApi/Startup.cs
@@ -134,7 +134,7 @@ namespace PortalApi
 
             app.UseCors(builder =>
                 builder
-                .WithOrigins("http://localhost:3000")
+                .WithOrigins("http://localhost:8080")
                 .AllowAnyHeader()
                 .AllowAnyMethod()
                 .AllowCredentials()
