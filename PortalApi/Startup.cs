c0c47a26b9c67a8670b42bb586d816ef273467b2
Miłosz Mika <milosz.mika93@gmail.com>
2020-07-16 20:37:13 +0200

    Changed db connectionString -> delete ensure deleted

--- PortalApi/Startup.cs
+++ PortalApi/Startup.cs
@@ -92,7 +92,7 @@ namespace PortalApi
                 options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
             );
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-            var sqlProvider = _configuration.GetValue<string>("AppSettings:SqlProvider");
+            var sqlProvider = _configuration.GetValue<string>("AppSettings:FreeASPHosting");
             Console.WriteLine($"appsettings.SqlProvider: {sqlProvider}");
             services.AddDbContext<PortalContext>(options =>
             {
